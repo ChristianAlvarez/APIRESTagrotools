7e8e0cc4732fc2f1aa1a538383ca569dac69b93e
Christian Omar Alvarez Astete <christian.alvarez.a@gmail.com>
2017-02-09 19:42:05 -0300

    sexto commit

--- database/migrations/2017_02_08_194905_create_picking_table.php
+++ database/migrations/2017_02_08_194905_create_picking_table.php
@@ -14,13 +14,15 @@ class CreatePickingTable extends Migration
     public function up()
     {
         Schema::create('picking', function (Blueprint $table) {
-            $table->string('pers_id', 12)->primary();
+            $table->string('pers_id', 12);
             $table->string('cpny_id', 12)->references('cpny_id')->on('company');
             $table->string('pers_name', 160);
             $table->string('pick_password');
             $table->boolean('pick_active');
             $table->boolean('pick_record');
             $table->timestamps();
+
+            $table->primary(array('pers_id', 'cpny_id'));
         });
     }
 
