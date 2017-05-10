6b034fc059e96d08714ed498d7d44897d74ce75c
Christian Omar Alvarez Astete <christian.alvarez.a@gmail.com>
2017-05-10 11:03:04 -0300

    42 commit

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
-use App\UserPicking;
+use App\Picking;
 use App\Device;
 use App\Company;
 use App\Reap;
@@ -21,27 +21,23 @@ class SyncController extends Controller
     public function SyncUp(Request $request)
     {
         
-        $requests = $request->only('pers_id', 'password', 'devi_id', 'updated_at_company', 'updated_at_picking', 'updated_at_device', 'updated_at_reap', 'updated_at_detailsreap'); 
+        $requests = $request->only('pers_id', 'cpny_id', 'updated_at_company', 'updated_at_picking', 'updated_at_reap', 'updated_at_detailsreap'); 
 
         $Pers_id = $request->pers_id;
         $Cpny_id = $request->cpny_id;
-        $Devi_id = $request->devi_id;
         $Updated_at_company = $request->updated_at_company;
         $Updated_at_picking = $request->updated_at_picking;
-        $Updated_at_device = $request->updated_at_device;
         $Updated_at_reap = $request->updated_at_reap;
         $Updated_at_detailsreap = $request->updated_at_detailsreap;
 
         $data = [
             'Pers_id' => $Pers_id, 
             'Cpny_id' => $Cpny_id,
-            'Devi_id' => $Devi_id
         ]; 
 
         $rules = [
-            'Pers_id' => 'required|max:15|exists:userspicking,pers_id',
+            'Pers_id' => 'required|max:15|exists:picking,pers_id',
             'Cpny_id' => 'required|max:20',
-            'Devi_id' => 'required|max:50|exists:device,devi_id',
         ];
 
         $messages = [
@@ -50,9 +46,6 @@ class SyncController extends Controller
             'Pers_id.exists'   => 'pers_id - Identificador del usuario debe existir en tabla Userpicking',
             'Cpny_id.required' => 'cpny_id - Compañia es requerido',
             'Cpny_id.max'      => 'cpny_id - Compañia maximo de caracteres permitidos 20',
-            'Devi_id.required' => 'devi_id - Identificador del dispositivo es requerido',
-            'Devi_id.max'      => 'devi_id - Dispositivo maximo de caracteres permitidos 50',
-            'Devi_id.exists'   => 'devi_id - Identificador del dispositivo debe existir en tabla Device',
         ];       
 
         $validator = Validator::make($data, $rules, $messages);
@@ -63,24 +56,23 @@ class SyncController extends Controller
             ]);
         }else {
 
-            $Company = Company::where('pers_id', $Pers_id)
-                              ->where('cpny_id', $Cpny_id)
+            $Company = Company::where('cpny_id', $Cpny_id)
                               ->where('cpny_active', 1)
                               ->where('updated_at', '>', $Updated_at_company)
                               ->orderBy('updated_at', 'desc')
                               ->get();
 
             $Picking = Picking::where('pers_id', $Pers_id)
-                              ->where('uspi_active', 1)
+                              ->where('pick_active', 1)
                               ->where('updated_at', '>', $Updated_at_picking)
                               ->orderBy('updated_at', 'desc')
                               ->get();
 
-            $Device = Device::where('devi_id', $Devi_id)
+            /*$Device = Device::where('devi_id', $Devi_id)
                              ->where('devi_active', 1)
                              ->where('updated_at', '>', $Updated_at_device)
                              ->orderBy('updated_at', 'desc')
-                             ->get();
+                             ->get();*/
 
             $Reap = Reap::where('cpny_id', $Cpny_id)
                           ->where('pers_id', $Pers_id)
@@ -89,7 +81,6 @@ class SyncController extends Controller
                           ->get();
 
             $DetailsReap = DetailsReap::where('cpny_id', $Cpny_id)
-                                ->where('pers_id', $Pers_id)
                                 ->where('updated_at', '>', $Updated_at_detailsreap)
                                 ->orderBy('updated_at', 'desc')
                                 ->get();
@@ -103,7 +94,6 @@ class SyncController extends Controller
 
             $Data = [
                 'Picking' => $Picking,
-                'Device' => $Device,
                 'Company' => $Company,
                 'Reap' => $Reap,
                 'DetailsReap' => $DetailsReap
@@ -121,36 +111,35 @@ class SyncController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function Synchronized($Pers_id, 
-                                 $Cpny_id, 
-                                 $Devi_id,
-                                 $created_userpicking, 
-                                 $created_device, 
-                                 $created_userpickingcompany, 
-                                 $created_reap, 
-                                 $created_detailsreap)
+    public function Synchronized(Request $request)
     {
+
+      $requests = $request->only('pers_id', 'cpny_id', 'updated_at_company', 'updated_at_picking', 'updated_at_reap', 'updated_at_detailsreap'); 
+
+        $Pers_id = $request->pers_id;
+        $Cpny_id = $request->cpny_id;
+        $Updated_at_company = $request->updated_at_company;
+        $Updated_at_picking = $request->updated_at_picking;
+        $Updated_at_reap = $request->updated_at_reap;
+        $Updated_at_detailsreap = $request->updated_at_detailsreap;
+
         $data = [
 
             'Pers_id' => $Pers_id, 
             'Cpny_id' => $Cpny_id,
-            'Devi_id' => $Devi_id
 
-            'created_userpicking'        => $created_userpicking, 
-            'created_device'             => $created_device,
-            'created_userpickingcompany' => $created_userpickingcompany,
-            'created_reap'               => $created_reap,
-            'created_detailsreap'        => $created_detailsreap,
+            'created_userpicking'        => $Updated_at_picking, 
+            'created_userpickingcompany' => $Updated_at_company,
+            'created_reap'               => $Updated_at_reap,
+            'created_detailsreap'        => $Updated_at_detailsreap
         ]; 
 
         $rules = [
 
             'Pers_id' => 'required|max:12|exists:userspicking,pers_id',
             'Cpny_id' => 'required|max:20',
-            'Devi_id' => 'required|max:50|exists:device,devi_id',
 
             'created_userpicking'        => 'required|date',
-            'created_device'             => 'required|date',
             'created_userpickingcompany' => 'required|date',
             'created_reap'               => 'required|date',
             'created_detailsreap'        => 'required|date',
@@ -163,9 +152,6 @@ class SyncController extends Controller
             'Pers_id.exists'   => 'pers_id - Identificador del usuario debe existir en tabla Userpicking',
             'Cpny_id.required' => 'cpny_id - Compañia es requerido',
             'Cpny_id.max'      => 'cpny_id - Compañia maximo de caracteres permitidos 12',
-            'Devi_id.required' => 'devi_id - Identificador del dispositivo es requerido',
-            'Devi_id.max'      => 'devi_id - Dispositivo maximo de caracteres permitidos 50',
-            'Devi_id.exists'   => 'devi_id - Identificador del dispositivo debe existir en tabla Device',
 
             'created_userpicking.required'          => 'created_userpicking - Periodo tabla UserPicking es requerido',
             'created_userpicking.date'              => 'created_userpicking - Periodo tabla UserPicking debe ser formato Date valido',
@@ -191,37 +177,37 @@ class SyncController extends Controller
             ]);
         }else {
 
-            $UserPicking = UserPicking::where('pers_id', $Pers_id)
-                                        ->where('uspi_active', 1)
-                                        ->where('uspi_record', 0)
-                                        ->whereDate('created_at', '<=',  $created_userpicking)
-                                        ->update(['uspi_record' => 1]);
+            $UserPicking = Picking::where('pers_id', $Pers_id)
+                                        ->where('pick_active', 1)
+                                        ->where('pick_record', 0)
+                                        ->whereDate('updated_at', '<=',  $Updated_at_picking)
+                                        ->update(['pick_record' => 1]);
             
 
-            $Device = Device::where('devi_id', $Devi_id)
+            /*$Device = Device::where('devi_id', $Devi_id)
                              ->where('devi_active', 1)
                              ->where('devi_record', 0)
                              ->whereDate('created_at', '<=',  $created_device)
-                             ->update(['devi_record' => 1]);
+                             ->update(['devi_record' => 1]);*/
 
 
-            $UserPickingCompanys = UserPickingCompany::where('pers_id', $Pers_id)
+            $UserPickingCompanys = Company::where('pers_id', $Pers_id)
                                                         ->where('cpny_id', $Cpny_id)
                                                         ->where('cpny_active', 1)
                                                         ->where('cpny_record', 0)
-                                                        ->whereDate('created_at', '<=',  $created_userpickingcompany)
+                                                        ->whereDate('updated_at', '<=',  $Updated_at_company)
                                                         ->update(['cpny_record' => 1]);
 
             $Reaps = Reap::where('cpny_id', $Cpny_id)
                          ->where('pers_id', $Pers_id)
                          ->where('reap_record', 0)
-                         ->whereDate('created_at', '<=',  $created_reap)
+                         ->whereDate('updated_at', '<=',  $Updated_at_reap)
                          ->update(['reap_record' => 1]);
             
             $DetailsReap = DetailsReap::whereIn('reap_id', $reap->reap_id)
                                       ->whereIn('dere_status_card', 1)
                                       ->whereIn('dere_record', 0)
-                                      ->whereDate('created_at', '<=',  $created_detailsreap)
+                                      ->whereDate('updated_at', '<=',  $Updated_at_detailsreap)
                                       ->update(['dere_record' => 1]);
 
             return response()->json([
