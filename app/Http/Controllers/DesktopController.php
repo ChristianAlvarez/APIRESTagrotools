9c7f0c482b6cf75fea82d8812ad9374277229bb8
Christian Omar Alvarez Astete <christian.alvarez.a@gmail.com>
2017-07-04 16:52:23 -0400

    68 commit

--- app/Http/Controllers/DesktopController.php
+++ app/Http/Controllers/DesktopController.php
@@ -271,6 +271,7 @@ class DesktopController extends Controller
                         
                         if (count($Devicetoken) == 0)
                         {
+                            dd($Devicetoken);
                             $DeviceToken = new \App\DeviceToken();
                             $DeviceToken->devi_id = $device['devi_id'];
                             $DeviceToken->devi_active = 1;
