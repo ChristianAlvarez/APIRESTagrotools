5dc197f061babf497fe8032d0242296c009c4a45
christian <christian.alvarez.a@gmail.com>
2018-02-09 17:48:42 -0300

    Cambios

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -470,7 +470,52 @@ class MobileController extends Controller
                 }
                 else
                 {
-                    $DetailsReap =  DetailsReap::where(['reap_id' => $detailsreap['reap_id']])
+                    $detail = DetailsReap::where('pers_id', $detailsreap['pers_id'])
+                                         ->where('reap_id', $detailsreap['reap_id'])
+                                         ->where('cpny_id', $detailsreap['cpny_id'])
+                                         ->first();
+
+                    if ($detail)
+                    {
+                        $DetailsReap =  DetailsReap::where(['reap_id' => $detailsreap['reap_id']])
+                                           ->where(['cpny_id' => $detailsreap['cpny_id']])
+                                           ->where(['card_identification' => $detailsreap['card_identification']])
+                                           ->update(['pers_id' => $detailsreap['pers_id'],
+                                                     'pers_name' => $detailsreap['pers_name'],
+                                                     'quad_name' => $detailsreap['quad_name'],
+                                                     'dere_status_card' => $detailsreap['dere_status_card'],
+                                                     'dere_record' => $detailsreap['dere_record'],
+                                                     'row_mode'     => $detailsreap['row_mode'],
+                                                     'card_identification' => $detailsreap['card_identification'],
+                                                     'dere_update' => $detailsreap['dere_update'],
+                                                     'created_at' => $detailsreap['created_at'],
+                                                     'updated_at' => $detailsreap['updated_at'],
+                                                     'dere_obs' => $detailsreap['dere_obs']]);
+                    }
+                    else
+                    {
+
+                        $DetailsReap = new \App\DetailsReap();
+                        $DetailsReap->reap_id = $detailsreap['reap_id'];
+                        $DetailsReap->cpny_id = $detailsreap['cpny_id'];
+                        $DetailsReap->card_identification = $detailsreap['card_identification'];
+                        $DetailsReap->pers_id = $detailsreap['pers_id'];
+                        $DetailsReap->pers_name = $detailsreap['pers_name'];
+                        $DetailsReap->quad_name = $detailsreap['quad_name'];
+
+                        $DetailsReap->dere_status_card = $detailsreap['dere_status_card'];
+                        $DetailsReap->dere_record = $detailsreap['dere_record'];
+                        $DetailsReap->row_mode = $detailsreap['row_mode'];
+                        $DetailsReap->created_at = $detailsreap['created_at'];
+                        $DetailsReap->updated_at = $detailsreap['updated_at'];
+                        $DetailsReap->dtrp_line_number = -1;
+                        $DetailsReap->dere_update = $detailsreap['dere_update'];
+                        $DetailsReap->dere_obs = $detailsreap['dere_obs'];
+
+                        $DetailsReap->save();
+                    }    
+
+                    /*$DetailsReap =  DetailsReap::where(['reap_id' => $detailsreap['reap_id']])
                                            ->where(['cpny_id' => $detailsreap['cpny_id']])
                                            ->where(['card_identification' => $detailsreap['card_identification_old']])
                                            ->update(['pers_id' => $detailsreap['pers_id'],
@@ -484,6 +529,21 @@ class MobileController extends Controller
                                                      'created_at' => $detailsreap['created_at'],
                                                      'updated_at' => $detailsreap['updated_at'],
                                                      'dere_obs' => $detailsreap['dere_obs']]);
+
+                                           $DetailsReap =  DetailsReap::where(['reap_id' => $detailsreap['reap_id']])
+                                           ->where(['cpny_id' => $detailsreap['cpny_id']])
+                                           ->where(['card_identification' => $detailsreap['card_identification_old']])
+                                           ->update(['pers_id' => $detailsreap['pers_id'],
+                                                     'pers_name' => $detailsreap['pers_name'],
+                                                     'quad_name' => $detailsreap['quad_name'],
+                                                     'dere_status_card' => $detailsreap['dere_status_card'],
+                                                     'dere_record' => $detailsreap['dere_record'],
+                                                     'row_mode'     => $detailsreap['row_mode'],
+                                                     'card_identification' => $detailsreap['card_identification'],
+                                                     'dere_update' => $detailsreap['dere_update'],
+                                                     'created_at' => $detailsreap['created_at'],
+                                                     'updated_at' => $detailsreap['updated_at'],
+                                                     'dere_obs' => $detailsreap['dere_obs']]);*/
                 }
             }
 
