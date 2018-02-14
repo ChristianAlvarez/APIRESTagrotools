a3d3055fab5c7b91d7e68f4dbefb71b82a03e52b
christian <christian.alvarez.a@gmail.com>
2018-02-13 23:53:36 -0300

    Commit

--- app/Http/Controllers/DesktopController.php
+++ app/Http/Controllers/DesktopController.php
@@ -568,7 +568,7 @@ class DesktopController extends Controller
                                     ->where(['pers_id' => $detail['pers_id']])
                                     ->where(['cpny_id' => $detail['cpny_id']])
                                     ->where(['card_identification' => $detail['card_identification']])
-                                    ->update(['dere_record' => 0,
+                                    ->update(['dere_update' => 0,
                                               'dtrp_line_number' => $detail['dtrp_line_number']]);
             }   
     }
