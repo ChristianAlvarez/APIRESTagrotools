c3dca09e0e420f51772f2513d009090d2400020b
christian <christian.alvarez.a@gmail.com>
2018-02-22 13:48:47 -0300

    Agregamos tabla Sincronizations

---
+++ database/migrations/2018_02_22_164612_alter_movementreap_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterMovementreapTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movementreap', function (Blueprint $table) {
+           
+            $table->integer('synchronizations_id', 50)->references('synchronizations')->on('id');
+          
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('movementreap', function (Blueprint $table) {
+            $table->integer('synchronizations_id', 50)->references('synchronizations')->on('id');
+        });
+    }
+}
