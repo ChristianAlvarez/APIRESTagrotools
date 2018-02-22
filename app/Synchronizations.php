2e745a50ff0d5840da47cc50132b165b26f12083
christian <christian.alvarez.a@gmail.com>
2018-02-22 20:02:51 -0300

    Cambios en tabla synchronizations

--- app/Synchronizations.php
+++ app/Synchronizations.php
@@ -28,6 +28,19 @@ class Synchronizations extends Model
         'esdo_id',
         'created_at',
         'updated_at',
+
+        /*
+        'id',
+        'cpny_id',
+        'sync_date_transaction',
+        'sync_device_id',
+        'pers_id',
+        'sync_latitud',
+        'sync_longitude',
+        'esdo_id',
+        'created_at',
+        'updated_at',
+        */
     ];
 
     public function Movementreap(){
---
+++ database/migrations/2018_02_22_225059_alter_synchronizations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterSynchronizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('synchronizations', function (Blueprint $table) {
+            $table->renameColumn('dmrp_date_transaction', 'sync_date_transaction');
+            $table->renameColumn('latitud', 'sync_latitude');
+            $table->renameColumn('longitud', 'sync_longitude');
+            $table->renameColumn('dmrp_device_id', 'sync_device_id');   
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('synchronizations', function (Blueprint $table) {
+            $table->renameColumn('sync_date_transaction', 'dmrp_date_transaction');
+            $table->renameColumn('sync_latitude', 'latitud');
+            $table->renameColumn('sync_longitude', 'longitud');
+            $table->renameColumn('sync_device_id', 'dmrp_device_id'); 
+        });
+    }
+}
